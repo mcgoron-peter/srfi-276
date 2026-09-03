1d1001da7daea02a6180bc4d74418249dafdbf6c
Peter McGoron <code@mcgoron.com>
2026-09-02 21:50:06 -0400

    maximum and minimum procedures

---
+++ lib/srfi/276.maxmin.scm
@@ -0,0 +1,133 @@
+(define (flmax-filter-nans-binary x y)
+  (cond
+    ((flnan? x) y)
+    ((flnan? y) x)
+    ((and (eqv? x -0.0)
+	  (eqv? y +0.0))
+     +0.0)
+    ((fl<? x y) y)
+    (else x)))
+
+(define (flmax-filter-nans . args)
+  (if (null? args)
+      -inf.0
+      (fold flmax-filter-nans-binary
+	    (car args)
+	    (cdr args))))
+
+(define (flmin-filter-nans-binary x y)
+  (cond
+   ((flnan? x) y)
+   ((flnan? y) x)
+   ((and (fl=? x -0.0)
+	 (fl=? y +0.0))
+    -0.0)
+   ((fl<? x y) y)
+   (else x)))
+
+(define (flmin-filter-nans . args)
+  (if (null? args)
+      +inf.0
+      (fold flmin-filter-nans-binary
+	    (car args)
+	    (cdr args))))
+
+(define (flmax-binary x y)
+  (cond
+    ((flunordered? x y) +nan.0)
+    ((and (eqv? x -0.0)
+	  (eqv? y +0.0))
+     +0.0))
+    ((fl<? x y) y)
+    (else x)))
+
+(define (flmax . args)
+  (if (null? args)
+      -inf.0
+      (fold flmax-binary (car args) (car args))))
+
+(define (flmin-binary x y)
+  (cond
+    ((flunordered? x y) +nan.0)
+    ((and (eqv? x -0.0) (eqv? y +0.0))
+     -0.0)
+    ((fl<? x y) x)
+    (else y)))
+
+(define (flmin . args)
+  (if (null? args)
+      +inf.0
+      (fold flmin-binary (car args) (car args))))
+
+;;; XXX: What should the base case of flmax-abs and flmin-abs be?
+;;; The base should probably be +0.0 for both of them.
+
+(define (flmax-abs-filter-nans-binary x y)
+  (cond
+    ((flnan? x) y)
+    ((flnan? y) x)
+    (else
+     (let ((abs-x (flabs x))
+	   (abs-y (flabs y)))
+       (cond
+	((fl<? abs-x abs-y) y)
+	((fl>? abs-x abs-y) x)
+	(else (flmax-filter-nans-binary x y)))))))
+
+(define (flmax-abs-filter-nans . args)
+  (if (null? args)
+      +0.0
+      (fold flmax-abs-filter-nans-binary
+	    (car args)
+	    (cdr args))))
+
+(define (flmin-abs-filter-nans-binary x y)
+  (cond
+    ((flnan? x) y)
+    ((flnan? y) x)
+    (else
+     (let ((abs-x (flabs x))
+	   (abs-y (flabs y)))
+       (cond
+	 ((fl<? abs-x abs-y) x)
+	 ((fl>? abs-x abs-y) y)
+	 (else (flmin-filter-nans-binary x y)))))))
+
+(define (flmin-abs-filter-nans . args)
+  (if (null? args)
+      +0.0
+      (fold flmin-abs-filter-nans-binary
+	    (car args)
+	    (cdr args))))
+
+(define (flmax-abs-binary x y)
+  (cond
+    ((flunordered? x y) +nan.0)
+    (else
+     (let ((abs-x (flabs x))
+	   (abs-y (flabs y)))
+       (cond
+	 ((fl<? abs-x abs-y) y)
+	 ((fl>? abs-x abs-y) x)
+	 (else (flmax-binary x y)))))))
+
+(define (flmax-abs . args)
+  (if (null? args)
+      +0.0
+      (fold flmax-abs-binary (car args) (cdr args))))
+
+(define (flmin-abs-binary x y)
+  (cond
+    ((flunordered? x y) +nan.0)
+    (else
+     (let ((abs-x (flabs x))
+	   (abs-y (flabs y)))
+       (cond
+	 ((fl<? abs-x abs-y) y)
+	 ((fl>? abs-x abs-y) x)
+	 (else (flmin-binary x y)))))))
+
+(define (flmin-abs . args)
+  (if (null? args)
+      +0.0
+      (fold flmax-abs-binary (car args) (cdr args))))5~
